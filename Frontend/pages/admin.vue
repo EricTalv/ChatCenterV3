cdb3a0658e05e588a99f5f2b2f74e8f7672228e8
Eric Talviste <Eric.Talviste@tptlive.ee>
2019-03-13 17:39:12 +0200

    Added some design to admin

--- Frontend/pages/admin.vue
+++ Frontend/pages/admin.vue
@@ -1,6 +1,28 @@
 <template>
     <section class="container">
-        <h1>Admin Page</h1>
+        <div class="row">
+            <div class="col-md">
+                <fieldset class="et-Field">
+                    <legend class="et-Legend"><h1>Admin Page</h1></legend>
+                    <p class="lead">This is the description for the Admin page.</p>
+                </fieldset>
+            </div>
+            <div class="col-md">
+                <fieldset class="et-Field">
+                    <legend class="et-Legend"><h1>Control</h1></legend>
+                    <a href="#" class="w-100 btn disabled">Yikes</a>
+                </fieldset>
+            </div>
+            <div class="col-md">
+                <fieldset class="et-Field">
+                    <legend class="et-Legend"><h1>Data</h1></legend>
+                    <p>Retrieved data:</p>
+                    <ul>
+
+                    </ul>
+                </fieldset>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -11,5 +33,18 @@
 </script>
 
 <style scoped>
+
+    .et-Field {
+        border: 1px solid black;
+        padding: 10px;
+        margin: 0;
+        min-height: 250px;
+    }
+
+    .et-Field > legend {
+        width: auto;
+        margin-top: 30px;
+        padding: 5px;
+    }
 
 </style>
