23ffecfe7a93a08065e66063e6f89fef88b9ec84
EricTalv <eric.talviste@tptlive.ee>
2019-03-13 11:29:05 +0200

    added vuex

--- Backend/laravel-docker-install/laravel/routes/api.php
+++ Backend/laravel-docker-install/laravel/routes/api.php
@@ -16,6 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::get('test', function(){
     return ['test' => 'adasds'];
+});
+
+Route::get('test2', function(){
+    return ['test2' => 'ada2sds'];
 });
--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -9,9 +9,14 @@
 
 export default {
     mounted() {
+      this.$api.get('/test2').then((response)=>{
+        console.log(response.data);
+      }),
+
       this.$api.get('/test').then((response)=>{
         console.log(response.data);
       })
+
     }
 }
 </script>
