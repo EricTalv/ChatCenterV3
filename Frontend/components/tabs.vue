6d0271bff519b032d85a73f3bee5501847ba4739
EricTalv <eric.talviste@tptlive.ee>
2019-03-27 10:18:38 +0200

    working with vuetify style

--- Frontend/components/tabs.vue
+++ Frontend/components/tabs.vue
@@ -12,16 +12,16 @@
                     ripple
             >
                 Item {{ n }}
-
             </v-tab>
             <v-tab-item
                     v-for="n in 3"
                     :key="n"
             >
-                <v-card flat>
-                    <v-card-text>{{ text }}</v-card-text>
-                </v-card>
+                <div>
+                    <slot>{{ text }}</slot>
+                </div>
             </v-tab-item>
+
         </v-tabs>
 
         <div class="text-xs-center mt-3">
@@ -33,14 +33,14 @@
 <script>
     export default {
         name: "tab",
-        data () {
+        data() {
             return {
                 active: null,
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
             }
         },
         methods: {
-            next () {
+            next() {
                 const active = parseInt(this.active)
                 this.active = (active < 2 ? active + 1 : 0)
             }
