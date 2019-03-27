b5d99a572b9febed54c59a1ccaf3f5ca94dc876f
EricTalv <eric.talviste@tptlive.ee>
2019-03-27 10:43:30 +0200

    finished design for tab

--- Frontend/components/tabs.vue
+++ Frontend/components/tabs.vue
@@ -1,19 +1,17 @@
 <template>
     <div>
-        <v-tabs
+        <v-tabs class="et-Tabs-container"
                 v-model="active"
                 color="dark"
-                dark
-                slider-color="red"
+                slider-color="black"
+                centered
+                grow
         >
-            <v-tab>
+            <v-tab class="et-Tab">
                 Cool item 1
             </v-tab>
-            <v-tab>
+            <v-tab class="et-Tab">
                 Cool item 2
-            </v-tab>
-            <v-tab>
-                Cool item 3
             </v-tab>
 
             <v-tab-item>
@@ -26,36 +24,20 @@
                     <v-card-text>For item 2</v-card-text>
                 </v-card>
             </v-tab-item>
-            <v-tab-item>
-                <v-card flat>
-                    <v-card-text>For item 3</v-card-text>
-                </v-card>
-            </v-tab-item>
+
         </v-tabs>
 
-        <div class="text-xs-center mt-3">
-            <v-btn @click="next">next tab</v-btn>
-        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "tab",
-        data() {
-            return {
-                active: null,
-            }
-        },
-        methods: {
-            next() {
-                const active = parseInt(this.active)
-                this.active = (active < 2 ? active + 1 : 0)
-            }
-        }
     }
 </script>
 
 <style scoped>
-
+    .et-Tab {
+        font-family: "Lucida Sans Typewriter", sans-serif;
+    }
 </style>
