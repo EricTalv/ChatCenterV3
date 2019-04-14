d3e57f8604bd5cfdb9ff3512946940eb96bd8b38
erictalv <eric.talviste@tptlive.ee>
2019-04-14 14:03:34 +0300

    removed stuff

--- Backend/laravel/routes/api.php
+++ Backend/laravel/routes/api.php
@@ -17,12 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('test', function () {
-    return ['test' => 'adasds'];
-});
-
-Route::get('test2', function () {
-    return ['test2' => 'ada2sds'];
-});
 
 Route::get('content-data', 'ContentsController@GetAllContents');
