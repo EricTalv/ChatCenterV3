4342081bfdfcc65edbce9f1142cb8ee860d0505a
erictalv <eric.talviste@tptlive.ee>
2019-04-14 16:12:54 +0300

    configuring the login controller api4

--- Backend/laravel/app/Http/Controllers/Auth/LoginController.php
+++ Backend/laravel/app/Http/Controllers/Auth/LoginController.php
@@ -66,25 +66,41 @@ class LoginController extends Controller
             ]);
         }
 
-        try {
-            if (!$token = $this->auth->attempt($request->only('email', 'password'))) {
-                return response()->json([
-                    'success' => false,
-                ]);
-            }
-        } catch (JWTException $e) {
-
-        }
-
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
         // If the login attempt was unsuccessful we will increment the number of attempts
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
         $this->incrementLoginAttempts($request);
 
-        return $this->sendFailedLoginResponse($request);
+        try {
+            if (!$token = $this->auth->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'success' => false,
+                    'error' => [
+                        'email' => [
+                            "Invalid email or password"
+                        ]
+                    ]
+                ], 422);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'error' => [
+                    'email' => [
+                        "Invalid email or password"
+                    ]
+                ]
+            ], 422);
+        }
+
+        return response()->json([
+            'success' => false,
+            'error' => [
+                'email' => [
+                    "Invalid email or password"
+                ]
+            ]
+        ], 422);
+
     }
 }
