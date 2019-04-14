943dbd28c9c6336e737ebef2f9a7e0681af2b08c
erictalv <eric.talviste@tptlive.ee>
2019-04-14 18:35:37 +0300

    me Logout

--- Backend/laravel/app/Http/Controllers/MeController.php
+++ Backend/laravel/app/Http/Controllers/MeController.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTAuth;
 
 class MeController extends Controller
 {
+    protected $auth;
+
+    public function _construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function index(Request $request)
     {
         return response()->json([
             'success' => true,
             'data' => $request->user()
         ]);
+    }
+
+    public function logout()
+    {
+        $this->auth->invalidate();
+
+        return response()->json([
+            'success' => true,
+        ]);
+
     }
 }
