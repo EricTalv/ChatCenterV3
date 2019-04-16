c2c47f4c04f8cddf9b8f411d03efd50508a6af37
erictalv <eric.talviste@tptlive.ee>
2019-04-16 18:10:45 +0300

    re doing register controller

--- Backend/laravel/app/Http/Controllers/Auth/RegisterController.php
+++ Backend/laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -1,16 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
-
 use App\User;
 use App\Http\Controllers\Controller;
-//use http\Env\Request; what the fuck is this ?!
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Tymon\JWTAuth\JWTAuth;
-
 class RegisterController extends Controller
 {
     /*
@@ -23,55 +17,22 @@ class RegisterController extends Controller
     | provide this functionality without requiring any additional code.
     |
     */
-
     use RegistersUsers;
-
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
-    protected $redirectTo = '';
-    protected $auth;
-
+    protected $redirectTo = '/home';
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(JWTAuth $auth)
+    public function __construct()
     {
-        $this->auth = $auth;
+        $this->middleware('guest');
     }
-
-    /**
-     * Handle a registration request for the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function register(Request $request)
-    {
-        $validator = $this->validator($request -> all());
-        if (!$validator -> fails()) {
-
-            $user = $this->create($request->all());
-            $token = $this->auth->attempt($request->only('email', 'password'));
-
-            return response()->json([
-               'success' => true,
-               'data' => $user,
-               'token' => $token,
-            ], 200);
-        }
-
-        return response()->json([
-            'success' => false,
-            'errors' => $validator->errors()
-        ]);
-    }
-
-
     /**
      * Get a validator for an incoming registration request.
      *
@@ -83,10 +44,9 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
-
     /**
      * Create a new user instance after a valid registration.
      *
