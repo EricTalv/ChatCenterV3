f46d9805eba184fbf88decd27f15f9104c3df785
erictalv <eric.talviste@tptlive.ee>
2019-04-18 11:18:29 +0300

    working on register errors

--- Frontend/pages/auth/register.vue
+++ Frontend/pages/auth/register.vue
@@ -18,9 +18,11 @@
                                 <v-text-field v-model="form.email" label="Email" name="email" prepend-icon="email"
                                               type="text"></v-text-field>
                                 <v-divider></v-divider>
-                                <v-text-field v-model="form.password" id="password" label="Password" name="password" prepend-icon="lock"
+                                <v-text-field v-model="form.password" id="password" label="Password" name="password"
+                                              prepend-icon="lock"
                                               type="password"></v-text-field>
-                                <v-text-field v-model="form.passwordConfirm" id="passwordConfirm" label="Confirm Password" name="passwordConfirm" prepend-icon="lock"
+                                <v-text-field v-model="form.passwordConfirm" id="passwordConfirm"
+                                              label="Confirm Password" name="passwordConfirm" prepend-icon="lock"
                                               type="password"></v-text-field>
                             </v-form>
                         </v-card-text>
@@ -28,6 +30,16 @@
                             <v-spacer></v-spacer>
                             <v-btn v-on:click="register" dark>Register</v-btn>
                         </v-card-actions>
+                        <v-slide-y-transition v-if="errorsList">
+                            <v-alert
+                                    v-for="error in errorsList"
+                                    :item="error"
+                                    :value="true"
+                                    type="error"
+                            >
+                                Error: {{ error }}
+                            </v-alert>
+                        </v-slide-y-transition>
                     </v-card>
                 </v-flex>
             </v-layout>
@@ -45,21 +57,28 @@
                     email: '',
                     password: '',
                     password_confirmation: '',
-                }
+                },
+                errorsList: null,
+
             }
         },
         methods: {
-            async register(){
-                await this.$axios.post('/register', this.form);
+            async register() {
+                this.$axios.post('/register', this.form).then((resp) => {
+                    console.log(resp);
+                }).catch((err) => {
+                    console.log(err.response.data.errors);
+                    this.errorsList = err.response.data.errors;
 
-                this.$auth.login({data: this.form});
 
-                this.$router.push('/');
+                });
+                /*
+                                this.$auth.login({data: this.form});
+
+                                this.$router.push('/');*/
             }
         },
-        computed:{
-
-        }
+        computed: {}
     }
 </script>
 
