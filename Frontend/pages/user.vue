d7cf7f972c7caf74b4e1e2fb47639f4cee68427b
erictalv <eric.talviste@tptlive.ee>
2019-05-01 16:17:14 +0300

    workin on user page

--- Frontend/pages/user.vue
+++ Frontend/pages/user.vue
@@ -1,9 +1,9 @@
 <template>
     <v-app>
         <v-navigation-drawer
-                v-model="drawer"
-                fixed
                 app
+                fixed
+                v-model="drawer"
         >
             <v-list dense>
                 <v-list-tile @click="">
@@ -16,22 +16,56 @@
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
-        <v-toolbar dark fixed app>
+        <v-toolbar app dark fixed>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title>Application</v-toolbar-title>
+            <v-toolbar-title>Your Profile</v-toolbar-title>
         </v-toolbar>
         <v-content>
-            <v-container fluid fill-height>
+            <v-container>
                 <v-layout
-                        justify-center
                         align-center
+                        justify-center
                 >
-                    <v-flex text-xs-center>
+                    <v-flex>
+                        <v-card >
+                            <v-card-title><h4>John</h4></v-card-title>
+                            <v-divider></v-divider>
+                            <v-list dense>
+                                <v-list-tile>
+                                    <v-list-tile-content>
+                                        Email:
+                                    </v-list-tile-content>
+                                    <v-list-tile-content align-end>
+                                        John@ef.com
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                                <v-list-tile>
+                                    <v-list-tile-content>
+                                        Created at:
+                                    </v-list-tile-content>
+                                    <v-list-tile-content align-end>
+                                        12:00
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                                <v-list-tile>
+                                    <v-list-tile-content>
+                                        Updated at:
+                                    </v-list-tile-content>
+                                    <v-list-tile-content align-end>
+                                        15:00
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </v-list>
+
+
+                        </v-card>
+
+
                     </v-flex>
                 </v-layout>
             </v-container>
         </v-content>
-        <v-footer dark app>
+        <v-footer app dark>
             <span class="white--text">&copy; 2019</span>
         </v-footer>
     </v-app>
