f3f86ddc62f160a470224d238ea7cffe424b54f5
erictalv <eric.talviste@tptlive.ee>
2019-05-02 13:20:27 +0300

    setting up speeddial

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -9,23 +9,23 @@
                         centered
                         grow
                         v-model="model">
-                        <v-tab :href="`#posts`" >Posts</v-tab>
-                        <v-tab :href="`#chats`" >Chats</v-tab>
+                    <v-tab :href="`#posts`">Posts</v-tab>
+                    <v-tab :href="`#chats`">Chats</v-tab>
                 </v-tabs>
 
                 <v-tabs-items v-model="model">
                     <v-tab-item
-                        lazy
-                        :key="1"
-                        :value="`posts`"
+                            lazy
+                            :key="1"
+                            :value="`posts`"
                     >
                         <div class="display-1 text-center ma-5">Posts</div>
                         <post_list></post_list>
                     </v-tab-item>
                     <v-tab-item
-                        lazy
-                        :key="2"
-                        :value="`chats`"
+                            lazy
+                            :key="2"
+                            :value="`chats`"
                     >
                         <div class="display-1 text-center ma-5">Chats</div>
                         <fieldset>
@@ -34,24 +34,60 @@
                         </fieldset>
                     </v-tab-item>
                 </v-tabs-items>
-<!--
-                <v-btn
-                        small
-                        top
-                        right
-                        fab
-                        fixed
-                        outline
-                        href="auth/login"
+                <!--
+                                <v-btn
+                                        small
+                                        top
+                                        right
+                                        fab
+                                        fixed
+                                        outline
+                                        href="auth/login"
+                                >
+                                    <v-icon>input</v-icon>
+
+                                    <v-speed-dial>
+
+                                    </v-speed-dial>
+                                </v-btn>-->
+
+                <v-speed-dial
+                        v-model="fab"
+                        :top="top"
+                        :bottom="bottom"
+                        :right="right"
+                        :left="left"
+                        :direction="direction"
+                        :open-on-hover="hover"
+                        :transition="transition"
+                        :fixed="fixed"
                 >
-                    <v-icon>input</v-icon>
-
-                    <v-speed-dial>
-
-                    </v-speed-dial>
-                </v-btn>-->
-
-
+                    <v-btn
+                            slot="activator"
+                            color="blue darken-2"
+                            dark
+                            fab
+                            hover
+                            v-model="fab"
+                    >
+                        <v-icon>account_circle</v-icon>
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                    <v-btn
+                            dark
+                            fab
+                            small
+                    >
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                    <v-btn
+                            dark
+                            fab
+                            small
+                    >
+                        <v-icon>add</v-icon>
+                    </v-btn>
+                </v-speed-dial>
 
 
             </v-flex>
@@ -69,13 +105,27 @@
         name: "index",
         components: {post_list},
 
+        data: () => ({
+            direction: 'bottom',
+            fab: true,
+            fling: false,
+            hover: false,
+            tabs: null,
+            top: true,
+            right: true,
+            bottom: false,
+            left: false,
+            fixed: true,
+            transition: 'slide-y-reverse-transition'
+        }),
+
         data() {
 
             return {
                 model: 'posts'
             }
         },
-   }
+    }
 </script>
 
 <style>
