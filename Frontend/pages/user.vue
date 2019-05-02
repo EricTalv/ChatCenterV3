339e16821b2484db109305e992e8dda3ec3737d6
erictalv <eric.talviste@tptlive.ee>
2019-05-02 11:32:01 +0300

    working on user page

--- Frontend/pages/user.vue
+++ Frontend/pages/user.vue
@@ -1,13 +1,59 @@
 <template>
-    <div></div>
+    <v-app>
+        <v-navigation-drawer
+                v-model="drawer"
+                fixed
+                app
+        >
+            <v-list dense>
+                <v-list-tile to="/">
+                    <v-list-tile-action>
+                        <v-icon>home</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content >
+                        <v-list-tile-title>Home</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile to="/">
+                    <v-list-tile-action>
+                        <v-icon>logout</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content >
+                        <v-list-tile-title>Logout</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout
+                        justify-center
+                        align-center
+                >
+                    <v-flex text-xs-center>
+
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+        <v-footer dark app>
+            <span class="white--text">&copy; 2019</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
     export default {
-        name: "user"
+        name: "user",
+        data: () => ({
+            drawer: null,
+        }),
+        props: {
+            source: String
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
